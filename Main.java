94080451a502fe28d63b15c129f987c017389f1d
Powerlifter2137 <keygle@protonmail.com>
2024-05-05 09:18:19 +0200

    zad3

--- Main.java
+++ Main.java
@@ -3,26 +3,18 @@ import company.models.Worker;
 
 public class Main {
     public static void main(String[] args) {
-        Worker worker1 = new Worker("Kazik", 1900, 1);
-        Worker worker2 = new Worker("Adi", 3800, 2);
-        Worker worker3 = new Worker("Tomek", 2900, 3);
-        Worker worker4 = new Worker("Pawel", 2500, 4);
+        Worker worker1 = new Worker("Tomek", 2100, 1, "2022-10-04", "Junior Developer");
+        Worker worker2 = new Worker("Kazik", 2800, 2, "2023-01-11", "Senior Developer");
+        Worker worker3 = new Worker("Alicja", 2900, 1, "2021-05-11", "Tester");
+        Manager manager = new Manager("Gawel", 6000, 3, "2022-07-12", "Department Head");
 
-        Manager manager = new Manager("Grzegorz", 6000, 5);
+        System.out.println(worker1.getName() + " has code: " + worker1.hashCode());
+        System.out.println(worker2.getName() + " has code: " + worker2.hashCode());
+        System.out.println(worker3.getName() + " has code: " + worker3.hashCode());
+        System.out.println(manager.getName() + " has code: " + manager.hashCode());
 
-        System.out.println("worker1 salary: " + worker1.getSalary());
-        worker1.work();
-
-        System.out.println("worker2 salary: " + worker2.getSalary());
-        worker2.work();
-
-        System.out.println("worker3 salary: " + worker3.getSalary());
-        worker3.work();
-
-        System.out.println("worker4 salary: " + worker4.getSalary());
-        worker4.work();
-
-        System.out.println("manager salary: " + manager.getSalary());
-        manager.work();
+        System.out.println(worker3.getName() + " equals worker1: " + worker3.equals(worker1));
+        System.out.println(worker3.getName() + " equals worker2: " + worker3.equals(worker2));
+        System.out.println(worker3.getName() + " equals manager: " + worker3.equals(manager));
     }
 }
